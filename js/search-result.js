dbf8c71d3d794c89207bb5f2350c4bc5e50af127
santinofeliu <sfeliu@udesa.edu.ar>
2025-11-20 08:55:44 -0300

    Corrigiendo el search-results hasta que funcione

--- js/search-result.js
+++ js/search-result.js
@@ -1,19 +1,24 @@
-let formularioBusqueda = document.querySelector('#forma');
+let formularioBusqueda = document.querySelector('.forma');
 let campoBusqueda = document.querySelector('.forma1');
-let categoria = document.querySelector('.categorias1');
+let mensajeError = document.querySelector('.mensaje-error');
 
-formularioBusqueda.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (campoBusqueda.value.length < 3 ){
-        return alert("El término buscado debe tener al menos 3 caracteres");
-    }
-    this.submit();
-});
+if (formularioBusqueda) {
+    formularioBusqueda.addEventListener('submit', function (e) {
+        let texto = campoBusqueda.value.trim();
 
+        if (texto === '') {
+            e.preventDefault();
+            alert('El campo de búsqueda no puede estar vacío.');
+        } else if (texto.length < 3) {
+            e.preventDefault();
+            alert('El término buscado debe tener al menos 3 caracteres.');
+        }
+ 
+    });
+}
 
 let queryString = window.location.search;
 let queryStringObj = new URLSearchParams(queryString);
-
 
 let terminoBuscado = queryStringObj.get('Resultados');
 
@@ -21,82 +26,141 @@ let tituloResultado = document.querySelector('.titulo-resultado');
 let metaResultados = document.querySelector('.Metaa');
 let contenedorResultados = document.querySelector('.resultados-busqueda');
 
-
 if (metaResultados) {
     metaResultados.innerText = '';
 }
+if (mensajeError) {
+    mensajeError.innerText = '';
+}
 
+if (terminoBuscado) {
+    terminoBuscado = terminoBuscado.trim();
+} else {
+    terminoBuscado = '';
+}
 
-if (!terminoBuscado || terminoBuscado.value === '') {
+if (terminoBuscado === '') {
     if (tituloResultado) {
         tituloResultado.innerText = 'No se ingresó ningún término de búsqueda.';
     }
-} else if (terminoBuscado.value().length < 3) {
+    if (mensajeError) {
+        mensajeError.innerText = 'Por favor ingresá un término en el buscador.';
+    }
+} else if (terminoBuscado.length < 3) {
+ 
     if (tituloResultado) {
         tituloResultado.innerText = 'El término buscado es demasiado corto.';
-    }
-    if (metaResultados) {
-        metaResultados.innerText = '';
     }
     if (mensajeError) {
         mensajeError.innerText = 'El término buscado debe tener al menos 3 caracteres.';
     }
 } else {
+    let terminoMinuscula = terminoBuscado.toLowerCase();
 
-    if (tituloResultado) {
-        tituloResultado.innerHTML = 'Resultados para: <mark>' + terminoBuscado + '</mark>';
-    }
+    if (terminoMinuscula.includes('vino')) {
 
-    fetch('https://dummyjson.com/products/search?q=' + terminoBuscado)
-        .then(function (respuesta) {
-            return respuesta.json();
-        })
-        .then(function (data) {
+        if (tituloResultado) {
+            tituloResultado.innerHTML = 'Resultados de búsqueda para: <mark>' + terminoBuscado + '</mark>';
+        }
+        let idsVinos = [1, 2, 3, 4, 5];
 
-            let resultados = data.products;
-
-            if (!resultados || resultados.length === 0) {
-                if (metaResultados) {
-                    metaResultados.innerText = 'Mostrando 0 resultados';
-                }
-                if (mensajeError) {
-                    mensajeError.innerText = 'No se encontraron productos para "' + terminoBuscado + '".';
-                }
-                return;
-            }
-
-            let cantidad = resultados.length;
+        if (idsVinos.length === 0) {
             if (metaResultados) {
-                metaResultados.innerText = 'Mostrando ' + cantidad + ' resultados';
+                metaResultados.innerText = 'Mostrando 0 resultados';
             }
-
-            let limite = 10;
-            if (cantidad < limite) {
-                limite = cantidad;
-            }
-
-            let html = '';
-
-            for (let i = 0; i < limite; i++) {
-                let producto = resultados[i];
-
-                html +=
-                    '<div class="items">' +
-                        '<img class="barato2" src="' + producto.thumbnail + '" alt="' + producto.title + '">' +
-                        '<p class="texto">$ ' + producto.price + '</p>' +
-                        '<p class="texto">' + producto.title + '</p>' +
-                        '<a class="detalles2" href="./product.html?id=' + producto.id + '">VER DETALLES</a>' +
-                    '</div>';
-            }
-
-            if (contenedorResultados) {
-                contenedorResultados.innerHTML = html;
-            }
-        })
-        .catch(function (error) {
-            console.log('Error al buscar productos:', error);
             if (mensajeError) {
-                mensajeError.innerText = 'Ocurrió un error al buscar los productos. Intentá nuevamente.';
+                mensajeError.innerText = 'No hay resultados para el término: ' + terminoBuscado;
             }
-        });
+            if (contenedorResultados) {
+                contenedorResultados.innerHTML = '';
+            }
+        } else {
+            if (metaResultados) {
+                metaResultados.innerText = 'Mostrando ' + idsVinos.length + ' resultados';
+            }
+            if (contenedorResultados) {
+                contenedorResultados.innerHTML = '';
+            }
+            for (let i = 0; i < idsVinos.length; i++) {
+                let idProducto = idsVinos[i];
+
+                fetch('https://dummyjson.com/products/' + idProducto)
+                    .then(function (respuesta) {
+                        return respuesta.json();
+                    })
+                    .then(function (producto) {
+                        let card =
+                            '<article class="items">' +
+                                '<img class="barato2" src="' + producto.thumbnail + '" alt="' + producto.title + '">' +
+                                '<p class="texto">$ ' + producto.price + '</p>' +
+                                '<p class="texto">' + producto.title + '</p>' +
+                                '<a class="detalles2" href="./product.html?id=' + producto.id + '">VER DETALLE</a>' +
+                            '</article>';
+
+                        if (contenedorResultados) {
+                            contenedorResultados.innerHTML += card;
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log('Error al cargar un vino:', error);
+                    });
+            }
+        }
+
+    } else {
+
+        if (tituloResultado) {
+            tituloResultado.innerHTML = 'Resultados de búsqueda para: <mark>' + terminoBuscado + '</mark>';
+        }
+
+        fetch('https://dummyjson.com/products/search?q=' + encodeURIComponent(terminoBuscado))
+            .then(function (respuesta) {
+                return respuesta.json();
+            })
+            .then(function (data) {
+                let resultados = data.products;
+
+                if (!resultados || resultados.length === 0) {
+                    if (metaResultados) {
+                        metaResultados.innerText = 'Mostrando 0 resultados';
+                    }
+                    if (mensajeError) {
+                        mensajeError.innerText = 'No hay resultados para el término: ' + terminoBuscado;
+                    }
+                    if (contenedorResultados) {
+                        contenedorResultados.innerHTML = '';
+                    }
+                    return;
+                }
+
+                let cantidad = resultados.length;
+                if (metaResultados) {
+                    metaResultados.innerText = 'Mostrando ' + cantidad + ' resultados';
+                }
+
+                let html = '';
+
+                for (let i = 0; i < resultados.length; i++) {
+                    let producto = resultados[i];
+
+                    html +=
+                        '<article class="items">' +
+                            '<img class="barato2" src="' + producto.thumbnail + '" alt="' + producto.title + '">' +
+                            '<p class="texto">$ ' + producto.price + '</p>' +
+                            '<p class="texto">' + producto.title + '</p>' +
+                            '<a class="detalles2" href="./product.html?id=' + producto.id + '">VER DETALLE</a>' +
+                        '</article>';
+                }
+
+                if (contenedorResultados) {
+                    contenedorResultados.innerHTML = html;
+                }
+            })
+            .catch(function (error) {
+                console.log('Error al buscar productos:', error);
+                if (mensajeError) {
+                    mensajeError.innerText = 'Ocurrió un error al buscar los productos. Intentá nuevamente.';
+                }
+            });
+    }
 }
