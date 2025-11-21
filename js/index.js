39ba038276c1aca2cde782b3673560f06f4ee88a
lgonzalo1205 <lgonzalo@udesa.edu.ar>
2025-11-21 12:32:07 -0300

    Product.js termiado

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ let formularioBusqueda = document.querySelector('#forma');
 let campoBusqueda = document.querySelector('.forma1');
 let categoria = document.querySelector('.categorias1');
 let lista = document.querySelector('.categorias2');
+let cuadro1 = document.querySelector('.cuadro1');
+let cuadro2 = document.querySelector(".cuadro2");
 
 formularioBusqueda.addEventListener('submit', function (e) {
     e.preventDefault();
-    if (campoBusqueda.value.length < 3 ){
+    if (campoBusqueda.value.length < 3) {
         return alert("El término buscado debe tener al menos 3 caracteres");
     }
     this.submit();
@@ -22,65 +24,67 @@ fetch('https://dummyjson.com/products/categories')
             lista.innerHTML += `<li><a href="./category.html?name=${data[i].name}">${data[i].name} </a></li>`;
         }
     })
-                
+
     .catch(function (error) {
         console.log("Error al traer las categorías", error);
     });
 
 
 
-if (lista) {
-    fetch('https://dummyjson.com/products/category-list')
-        .then(function(respuesta) {
-            return respuesta.json();
-        })
-        .then(function(data) {
-            let contenidoCategorias = '';
-            for (let i = 0; i < data.length; i++) {
-                let categoriaNombre = data[i];
-                
-                contenidoCategorias += '<li><a href="./category.html?categoria=' + categoriaNombre + '">' + categoriaNombre + '</a></li>';
-            }
-            lista.innerHTML = contenidoCategorias;
-        })
-        .catch(function(error) {
-            console.log("Error al traer las categorías", error);
-            if (lista) lista.innerHTML = '<li>Error al cargar categorías.</li>';
-        });
-}
-
-
-
-
-
-
-
-
-fetch('https://dummyjson.com/products/category/smartphones?limit=10') 
-    .then(function(respuesta) {
+fetch('https://dummyjson.com/products/category/smartphones?limit=10')
+    .then(function (respuesta) {
         return respuesta.json();
     })
-    .then(function(data) {
+    .then(function (data) {
+
+        let contenido = '';
+
+        for(let i = 0; i < data.products.length; i++) {
+            contenido += `
+             <article><img class="barato" src="${data.products[i].thumbnail}" alt="">
+                    <p class="texto">${data.products[i].title}</p>
+                    <p class="texto">${data.products[i].price}</p>
+                    <a class="detalles" href="./product.html?id=${data.products[i].id}">VER DETALLES</a>
+                    </article>
+            `
+        }
+
+        cuadro1.innerHTML = contenido;
+
+        console.log(contenido);
         
-        Productos2(data.products, '.cuadro1');
+
+        
     })
-    .catch(function(error) {
+    .catch(function (error) {
         console.log("Error al traer los smartphones:", error);
         if (document.querySelector('.cuadro1')) {
-             document.querySelector('.cuadro1').innerHTML = '<p>Error al cargar productos (smartphones): ' + error.message + '</p>';
+            document.querySelector('.cuadro1').innerHTML = '<p>Error al cargar productos (smartphones): ' + error.message + '</p>';
         }
     });
 
 
 fetch('https://dummyjson.com/products/category/groceries?limit=10')
-    .then(function(respuesta) {
+    .then(function (respuesta) {
         return respuesta.json();
     })
-    .then(function(data) {
+    .then(function (data) {
 
-        Productos2(data.products, '.cuadro2');
+        let contenido = '';
+
+         for(let i = 0; i < data.products.length; i++) {
+            contenido += `
+             <article><img class="barato" src="${data.products[i].thumbnail}" alt="">
+                    <p class="texto">${data.products[i].title}</p>
+                    <p class="texto">${data.products[i].price}</p>
+                    <a class="detalles" href="./product.html?id=${data.products[i].id}">VER DETALLES</a>
+                    </article>
+            `
+        }
+
+        cuadro2.innerHTML = contenido;
     })
-    .catch(function(error) {
+    .catch(function (error) {
         console.log("Error al traer las fragancias:", error);
         if (document.querySelector('.cuadro2')) {
             document.querySelector('.cuadro2').innerHTML = '<p>Error al cargar productos (fragancias): ' + error.message + '</p>';
